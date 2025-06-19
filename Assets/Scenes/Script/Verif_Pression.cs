4991f4a242de6bec229f494f2039f5c7ffd34a7b
Aurélien Dulaurier <adulaurier@laposte.net>
2025-06-19 15:35:47 +0200

    Plaque de pression + perso

---
+++ Assets/Scenes/Script/Verif_Pression.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressurePlateManager : MonoBehaviour
+{
+    public int totalPlates = 3; // Nombre total de plaques
+    private int activatedPlates = 0; // Compteur de plaques activées
+
+    public void ActivatePlate()
+    {
+        activatedPlates++;
+        if (activatedPlates >= totalPlates)
+        {
+            Teleporter();
+        }
+    }
+
+    void Teleporter()
+    {
+        SceneManager.LoadScene("NomDeVotreScene"); // Remplacez par le nom de la scène de destination
+    }
+}
