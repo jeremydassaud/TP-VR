c16d311e4c09639f0aaf7aefe9b4ea21598d23e3
Aurélien Dulaurier <adulaurier@laposte.net>
2025-06-19 15:00:00 +0200

    Salle + grille + prendre objet

---
+++ Assets/Scenes/Script/Gestion_Grille_Coffre.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Gestion_Grille_Coffre : MonoBehaviour
+{
+    public GameObject objet; // L'objet à rendre invisible
+    public bool AsCoffre = true; // La variable qui détermine la visibilité
+
+    void Update()
+    {
+        // Vérifie la valeur de la variable
+        if (AsCoffre)
+        {
+            RendreInvisible();
+        }
+        else
+        {
+            RendreVisible();
+        }
+    }
+
+    void RendreInvisible()
+    {
+        objet.SetActive(false); // Désactive l'objet
+        Collider collider = objet.GetComponent<Collider>();
+        if (collider != null)
+        {
+            collider.enabled = false; // Désactive le collider
+        }
+    }
+
+    void RendreVisible()
+    {
+        objet.SetActive(true); // Active l'objet
+        Collider collider = objet.GetComponent<Collider>();
+        if (collider != null)
+        {
+            collider.enabled = true; // Active le collider
+        }
+    }
+}
---
+++ Assets/Scenes/Script/Gestion_Grille_Ossement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Gestion_Grille_Ossement : MonoBehaviour
+{
+    public GameObject objet; // L'objet à rendre invisible
+    public bool AsOssement; // La variable qui détermine la visibilité
+
+    void Update()
+    {
+        // Vérifie la valeur de la variable
+        if (AsOssement)
+        {
+            RendreInvisible();
+        }
+        else
+        {
+            RendreVisible();
+        }
+    }
+
+    void RendreInvisible()
+    {
+        objet.SetActive(false); // Désactive l'objet
+        Collider collider = objet.GetComponent<Collider>();
+        if (collider != null)
+        {
+            collider.enabled = false; // Désactive le collider
+        }
+    }
+
+    void RendreVisible()
+    {
+        objet.SetActive(true); // Active l'objet
+        Collider collider = objet.GetComponent<Collider>();
+        if (collider != null)
+        {
+            collider.enabled = true; // Active le collider
+        }
+    }
+}
